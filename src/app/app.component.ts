24b9015a0366054dbc6c7f17cf2347363f929257
MangalaKulakarni <mangalabk77@gmail.com>
2021-06-22 15:40:38 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,26 @@
 import { Component, VERSION } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { noop } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  showForm = false;
+  projects = [];
+  projectForm: FormGroup;
+  
+  constructor() {
+    this.projectForm= new FormGroup({
+      projectName: new FormControl(null),
+      customerName: new FormControl(null),
+      location: new FormControl(null),
+    })  
+  }
+
+  onsubmit() {
+    this.projects.push(this.projectForm.value);
+  }
+}
